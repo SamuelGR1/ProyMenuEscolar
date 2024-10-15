66b00403cab05f1fe932d435d99a4d3bfdf07a4c
Samuel <samuelgeronimo.rosales@gmail.com>
2024-10-14 23:15:18 -0600

    fin curso

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -125,22 +125,22 @@ export async function deleteInvoice(id: string) {
   }
 }
 
-  export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData,
-  ) {
-    try {
-      await signIn('credentials', formData);
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case 'CredentialsSignin':
-            return 'Invalid credentials.';
-          default:
-            return 'NO LE SOBAS.';
-        }
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
       }
-      throw error;
     }
+    throw error;
   }
+}
   
